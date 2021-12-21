0fb8eaa616527882a307963376cbf27ad38c030a
rangjin <46679581+rangjin@users.noreply.github.com>
2021-12-21 18:43:51 +0900

    Initial commit

---
+++ src/main/java/com/rangjin/twelvejanggi/TwelveJanggiApplication.java
@@ -0,0 +1,13 @@
+package com.rangjin.twelvejanggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwelveJanggiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwelveJanggiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rangjin/twelvejanggi/TwelveJanggiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rangjin.twelvejanggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwelveJanggiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
